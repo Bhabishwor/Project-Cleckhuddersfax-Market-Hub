33de5dbe00eaed339c599354aa41828c81672c5b
Frozen-369 <frozenboyarp@gmail.com>
2024-04-09 07:32:07 +0545

    Completed Login,Register for Customer and header

---
+++ Execution/PHP/header.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <title>DashBoard</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" />
+  <link rel="stylesheet" href="../CSS/header.css">
+</head>
+
+<body>
+
+
+  <header>
+
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark custom-navbar">
+      <div class="container-fluid">
+        <a class="navbar-brand logo" href="#"><img src="../Image/Logo.png" alt="Logo" height="60" width="80"></a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo02"
+          aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
+          <ul class="navbar-nav me-auto mb-2 mb-lg-0 custom-nav-links">
+            <li class="nav-item active">
+              <a class="nav-link" href="#">Home</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Category</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link roleBasedRedirect" href="#">About Us</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link roleBasedRedirect" href="#">Contact Us</a>
+            </li>
+
+          </ul>
+          <div class="login_register">
+            <div class="login">
+              <i class="fa-regular fa-user" style="color: white;"></i>
+              <p class="user">LOGIN</p>
+            </div>
+          </div>
+          <div class="cart">
+            <div class="login">
+            <i class="fa-solid fa-cart-shopping" style="color: white;"></i>
+              <p class="cart">CART</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </nav>
+  </header>
+
+
+
+
+
+  <script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL"
+    crossorigin="anonymous"></script>
+  <script src="main.js"></script>
+</body>
+
+</html>
---
+++ Execution/PHP/login.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login System</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" />
+
+    <link rel="stylesheet" href="../CSS/styles.css">
+</head>
+
+<body>
+    <form method="post" id="employeeLogin">
+        <h2 class="fw-bolder text_center mt-3 ">LOGIN</h2>
+        <div class="container border border-2 rounded color_white pb-4">
+
+            <div class="login_container mt-4">
+                <div class="Username">
+                    <label>Username</label>
+                    <div class="input-container">
+                        <i class="icon fas fa-user"></i>
+                        <input type="text" class="text_box" placeholder="Username" name="username">
+                        <hr>
+                    </div>
+                </div>
+
+                <div class="password">
+                    <label>Password</label>
+                    <div class="input-container">
+                        <i class="icon fas fa-lock"></i>
+                        <input type="password" class="text_box" placeholder="Password" name="password">
+                        <hr>
+                    </div>
+                </div>
+
+                <label>
+                    <input type="checkbox">
+                    Remember Me
+                </label>
+
+                <!-- <label><u>Forgot Password?</u></label> -->
+                <a href="" id="forgot_password">Forgot Password?</a>
+
+
+            </div>
+            <div class="error" id="error_employeeLogin"></div>
+            <div class="login_container mt-3">
+                <button type="submit" class="btn btn-dark rounded-5 mb-4">Login</button>
+
+                <button type="button" class="btn btn-dark rounded-5">
+                    <a href="register.php" class="remove">Register</a>
+                </button>
+
+            </div>
+            <div class="logo">
+                <p>Sign in Using</p>
+                <i class="fa-brands fa-facebook" style="color: #4267B2;"></i>
+                <i class="fa-brands fa-twitter" style="color: #1da1f2;"></i>
+                <i class="fa-brands fa-google-plus" style="color: #DB4A39;"></i>
+            </div>
+
+        </div>
+    </form>
+    <script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL"
+        crossorigin="anonymous"></script>
+    <script src="script.js"></script>
+</body>
+
+</html>
