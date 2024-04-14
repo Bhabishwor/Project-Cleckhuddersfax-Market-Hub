49b0fad342cb7e8cbee3fbd0c9ae8e8f783433c1
Bhabishwor Gurung <gbhabishwor22@tbc.edu.np>
2024-04-14 20:18:46 +0545

    Added footer

---
+++ Execution/PHP/footer.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html lang="en" class="bg-dark">
+    
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    
+    <link rel="stylesheet" href="../CSS/styles.css">
+</head>
+
+<footer class="footer container-fluid p-5 bg-light">
+    <div class="row">
+        <div class="col-sm px-5">
+            <p><b>Location</b></p>
+            <a href="https://maps.app.goo.gl/AGGWoHjje36mcPnGA" target="_blank"><img src="../Image/map.png" alt="Collection Point Location" width="464" height="250"></a>
+        </div>
+
+        <div class="col-sm px-1">
+            <p><b>Shop</b></p>
+            <p><a href="" >Shop 1</a></p>
+            <p><a href="" >Shop 2</a></p>
+            <p><a href="" >Shop 3</a></p>
+            <p><a href="" >Shop 4</a></p>
+            <p><a href="" >Shop 5</a></p>
+            <p><a href="" >Shop 6</a></p>
+            <p><a href="" >Shop 7</a></p>
+            <p><a href="" >Shop 8</a></p>
+            <p><a href="" >Shop 9</a></p>
+            <p><a href="" >Shop 10</a></p>
+        </div>
+        
+        <div class="col-sm px-1">
+            <p><b>Help</b></p>
+            <p><a href="" >Overview</a></p>
+            <p><a href="" >Become a Trader</a></p>
+            <p><a href="" >FAQs</a></p>
+        </div>
+        
+        <div class="col-sm px-1">
+            <p><b>About</b></p>
+            <p><a href="" >About Us</a></p>
+            <p><a href="" >Terms & Conditions</a></p>
+            <p><a href="" >Team Community Harvest</a></p>
+        </div>
+        
+        <div class="col-sm px-1">
+            <p><b>Contact Us</b></p>
+            <p><a href="contact_us.php" >Contact Us</a></p>
+            <p><a href="">Email</a></p>
+        </div>
+        
+        <div class="col-sm px-1">
+            <p><b>Social</b></p>
+            <a href="https://facebook.com" target="_blank">
+                <img src="../Image/facebook_icon.png" alt="Facebook" width="50" height="50">
+            </a>
+            <br>
+            <a href="https://instagram.com" target="_blank">
+            <img src="../Image/instagram_icon.png" alt="Instagram" width="50" height="50">
+            </a>
+            <br>
+            <a href="https://twitter.com" target="_blank">
+            <img src="../Image/x_icon.png" alt="X" width="50" height="50">
+            </a>
+        </div>
+    </div>
+</footer>
+</html>
