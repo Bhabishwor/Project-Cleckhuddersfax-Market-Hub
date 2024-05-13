46bfcf6c3416387c5e37e584e0c452ce428b5e5b
Bhabishwor Gurung <gbhabishwor22@tbc.edu.np>
2024-05-13 15:41:52 +0545

    Added about us page, modified homepage and footer

---
+++ Execution/PHP/about_us.php
@@ -0,0 +1,157 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+    <title>About Us | Cleckhuddersfax Market Hub</title>
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+
+    <style>
+        @import url('https://fonts.googleapis.com/css?family=Montserrat&display=swap');
+
+        body {
+            font-family: 'Montserrat', sans-serif;
+        }
+        
+        .about-title {
+            font-size: 35px;
+            font-weight: bold;
+            color: #3e4652;
+            margin-top: 30px;
+            text-align: center;
+        }
+
+        .about-text {
+            font-size: 16px;
+            color: #3e4652;
+            margin-top: -20px;
+            text-align: justify;
+        }
+
+        .bg-category {
+            background-image: url("../Image/container2.png");
+            background-size: cover; /* Cover the entire background */
+            background-position: center; /* Center the background */
+            background-repeat: no-repeat; /* Prevent the background from repeating */
+        }
+
+        .member-name {
+            font-size: 27px;
+            font-weight: bold;
+            color: #3e4652;
+            margin-top: 30px;
+        }
+
+        .member-info {
+            font-size: 16px;
+            color: #3e4652;
+            margin-top: -20px;
+            text-align: justify;
+        }
+    </style>
+</head>
+
+<body>
+    <?php include "header.php"; ?>
+    
+    <div class="container-fluid p-5">
+            <p class="about-title">OUR STORY</p>
+            <br><br><br>
+            <div class="about-text">
+                <div class="row">
+                    <p class="col">Local small independent businesses recognize the need to adapt to the changing consumer behaviour in order to compete with national chains. Despite several opportunities for locals to purchase at larger supermarkets, small businesses continue to prosper. However, with the introduction of convenience shops by national chains and a plan for a larger store, local businesses see the need to innovate in order to keep their consumer base. They acknowledge that a lot of  locals are interested in shopping with them but aren’t able to do so since their working hours coincide with the store’s opening hours. As a result there is an obvious need to investigate alternate shopping techniques that fit the schedules of targeted end users.</p>
+                    <p class="col">As a practical solution to the issue of losing local shops in Cleckhuddersfax to national chains, we, Team Community Harvest, created an online platform for the residents. With round-the-clock accessibility, users can easily browse and purchase a wide variety of products, while merchants can benefit from valuable sales insights. The system not only saves time but also strengthens the sense of community by keeping families together and eliminating the need for travel. Overall, it transforms conventional shopping methods, providing a more convenient and connected experience for the Cleckhuddersfax community.</p>
+                </div>
+            </div>
+    </div>
+
+    <p class="about-title">OUR TEAM</p>
+
+    <div class="container p-5 offset-md-1">
+        <div class="row">
+            <div class="col-md-6">
+                <img src="../Image/team/Sanskriti.jpeg" alt="Sanskriti Dhungel" width="250" height="300">
+            </div>
+
+            <div class="col-md-6">
+                <p class="member-name">Sanskriti Dhungel</p>
+                <p class="member-info">Leader<br>Shaper, Complete Finisher, Specialist</p>
+            </div>
+        </div>
+    </div>
+
+    <div class="container p-5 offset-md-1">
+        <div class="row">
+            <div class="col-md-6">
+                <p class="member-name">Arpit Sharma</p>
+                <p class="member-info">Plant, Co-ordinator, Complete Finisher</p>
+            </div>
+
+            <div class="col-md-6">
+                <img src="../Image/team/Arpit.jpeg" alt="Arpit Sharma" width="250" height="300">
+            </div>
+        </div>
+    </div>
+    
+    <div class="container p-5 offset-md-1">
+        <div class="row">
+            <div class="col-md-6">
+                <img src="../Image/team/Aayusha.jpeg" alt="Aayusha Maharjan" width="250" height="300">
+            </div>
+
+            <div class="col-md-6">
+                <p class="member-name">Aayusha Maharjan</p>
+                <p class="member-info">Shaper, Complete Finisher, Specialist</p>
+            </div>
+        </div>
+    </div>
+
+    <div class="container p-5 offset-md-1">
+        <div class="row">
+            <div class="col-md-6">
+                <p class="member-name">Bhabishwor Gurung</p>
+                <p class="member-info">Monitor Evaluator, Complete Finisher, Teamworker</p>
+            </div>
+
+            <div class="col-md-6">
+                <img src="../Image/team/Bhabishwor.jpeg" alt="Bhabishwor Gurung" width="250" height="300">
+            </div>
+        </div>
+    </div>
+
+    <div class="container p-5 offset-md-1">
+        <div class="row">
+            <div class="col-md-6">
+                <img src="../Image/team/Sana.jpeg" alt="Sana Banu" width="250" height="300">
+            </div>
+
+            <div class="col-md-6">
+                <p class="member-name">Sana Banu</p>
+                <p class="member-info">Teamworker, Monitor Evaluator, Implementer</p>
+            </div>
+        </div>
+    </div>
+
+    <div class="container p-5 offset-md-1">
+        <div class="row">
+            <div class="col-md-6">
+                <p class="member-name">Sindhu Thapa Magar</p>
+                <p class="member-info">Resource Investigator, Implementer, Specialist</p>
+            </div>
+
+            <div class="col-md-6">
+                <img src="../Image/team/Sindhu.jpeg" alt="Sindhu Thapa Magar" width="250" height="300">
+            </div>
+        </div>
+    </div>
+
+    <div><img src="../Image/about us.png" alt="" class="container-fluid p-5"></div>
+
+    <?php include "footer.php"; ?>
+    
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+</body>
+</html>
--- Execution/PHP/footer.php
+++ Execution/PHP/footer.php
@@ -9,7 +9,7 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 
     <style>
-        .bg-container {
+        .bg-footer {
             background-image: url("../Image/container.png");
             background-size: cover; /* Cover the entire background */
             background-position: center; /* Center the background */
@@ -18,11 +18,11 @@
     </style>
 </head>
 
-<footer class="footer container-fluid bg-container px-5 pt-5 text-white">
+<footer class="footer container-fluid bg-footer px-5 pt-5 text-white">
     <div class="row">
         <div class="col-sm px-5">
             <p><b>Location</b></p>
-            <a href="https://maps.app.goo.gl/AGGWoHjje36mcPnGA" target="_blank"><img src="../Image/map.png" alt="Collection Point Location" width="464" height="250"></a>
+            <a href="https://maps.app.goo.gl/AGGWoHjje36mcPnGA" target="_blank"><img src="../Image/map.png" alt="Collection Point Location" width="371.2" height="200"></a>
         </div>
 
         <div class="col-sm px-1">
@@ -30,13 +30,6 @@
             <p>Shop 1</p>
             <p>Shop 2</p>
             <p>Shop 3</p>
-            <p>Shop 4</p>
-            <p>Shop 5</p>
-            <p>Shop 6</p>
-            <p>Shop 7</p>
-            <p>Shop 8</p>
-            <p>Shop 9</p>
-            <p>Shop 10</p>
         </div>
         
         <div class="col-sm px-1">
@@ -61,15 +54,15 @@
         <div class="col-sm px-1">
             <p><b>Social</b></p>
             <a href="https://facebook.com" target="_blank">
-                <img src="../Image/socials/facebook.png" alt="Facebook" width="38" height="38">
+                <img src="../Image/socials/facebook_white.png" alt="Facebook" width="38" height="38">
             </a>
             <br><br>
             <a href="https://instagram.com" target="_blank">
-                <img src="../Image/socials/instagram.png" alt="Instagram" width="38" height="38">
+                <img src="../Image/socials/instagram_white.png" alt="Instagram" width="38" height="38">
             </a>
             <br><br>
             <a href="https://twitter.com" target="_blank">
-                <img src="../Image/socials/x.png" alt="X" width="38" height="38">
+                <img src="../Image/socials/x_white.png" alt="X" width="38" height="38">
             </a><br>
         </div>
     </div>
