161bb1adf5ef7930ea225cc906e8ed9bae5af6a1
Sindhu Thapa Magar <msindhu22@tbc.edu.np>
2024-05-19 10:08:04 +0545

    Added cart, product details, trader shop and trader order

---
+++ Execution/PHP/Trader/trader_order.php
@@ -0,0 +1,126 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>order</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+  <link rel="stylesheet" href="./css/styles.css">
+</head>
+
+<body>
+  <div class="container py-5">
+    <h5>Order</h5>
+    <div class="wrapper">
+      <div class="box mb-5">
+        <div class="row row1">
+          <div class="col-8 col1">
+            <div class="row row2 my-2">
+              <div class="col-6 col3">
+                <h5>Lorem</h5>
+              </div>
+              <div class="col-6 col4">Collection slot: Wed 10-13</div>
+            </div>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Maxime molestiae eos consequatur tempore nulla
+              nemo suscipit, dicta ullam illo alias quasi saepe quis, nam autem vitae perspiciatis facilis corrupti
+              impedit.</p>
+          </div>
+          <div class="col-4 col2">
+            <div class="btn">
+              <button class="btn btn-dark">Complete</button>
+            </div>
+            <div class="menu">
+              <button class="btn btn-dark"><i class="bi bi-three-dots-vertical"></i></button>
+
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="box mb-5">
+        <div class="row row1">
+          <div class="col-8 col1">
+            <div class="row row2 my-2">
+              <div class="col-6 col3">
+                <h5>Lorem</h5>
+              </div>
+              <div class="col-6 col4">Collection slot: Wed 10-13</div>
+            </div>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Maxime molestiae eos consequatur tempore nulla
+              nemo suscipit, dicta ullam illo alias quasi saepe quis, nam autem vitae perspiciatis facilis corrupti
+              impedit.</p>
+          </div>
+          <div class="col-4 col2">
+            <div class="btn">
+              <button class="btn btn-dark">Complete</button>
+            </div>
+            <div class="menu">
+              <button class="btn btn-dark"><i class="bi bi-three-dots-vertical"></i></button>
+
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="box mb-5">
+        <div class="row row1">
+          <div class="col-8 col1">
+            <div class="row row2 my-2">
+              <div class="col-6 col3">
+                <h5>Lorem</h5>
+              </div>
+              <div class="col-6 col4">Collection slot: Wed 10-13</div>
+            </div>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Maxime molestiae eos consequatur tempore nulla
+              nemo suscipit, dicta ullam illo alias quasi saepe quis, nam autem vitae perspiciatis facilis corrupti
+              impedit.</p>
+          </div>
+          <div class="col-4 col2">
+            <div class="btn">
+              <button class="btn btn-dark">Complete</button>
+            </div>
+            <div class="menu">
+              <button class="btn btn-dark"><i class="bi bi-three-dots-vertical"></i></button>
+
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="box mb-5">
+        <div class="row row1">
+          <div class="col-8 col1">
+            <div class="row row2 my-2">
+              <div class="col-6 col3">
+                <h5>Lorem</h5>
+              </div>
+              <div class="col-6 col4">Collection slot: Wed 10-13</div>
+            </div>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Maxime molestiae eos consequatur tempore nulla
+              nemo suscipit, dicta ullam illo alias quasi saepe quis, nam autem vitae perspiciatis facilis corrupti
+              impedit.</p>
+          </div>
+          <div class="col-4 col2">
+            <div class="btn">
+              <button class="btn btn-dark">Complete</button>
+            </div>
+            <div class="menu">
+              <button class="btn btn-dark"><i class="bi bi-three-dots-vertical"></i></button>
+
+            </div>
+          </div>
+        </div>
+      </div>
+      
+
+    </div>
+
+  </div>
+
+
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
+    crossorigin="anonymous"></script>
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+</body>
+
+</html>
