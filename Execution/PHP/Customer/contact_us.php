6dbe7b7b92c0248cf7cc9a26f00e5e9ab9d42d31
Bhabishwor Gurung <gbhabishwor22@tbc.edu.np>
2024-05-20 10:05:09 +0545

    Modified login, contact_us, and customer_register

--- Execution/PHP/Customer/contact_us.php
+++ Execution/PHP/Customer/contact_us.php
@@ -3,34 +3,23 @@
 
 <head>
   <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Contact Us - Community Harvest</title>
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
-    integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css"
-    integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g=="
-    crossorigin="anonymous" referrerpolicy="no-referrer" />
-  <style>
-    body {
-      background-color: #f0f9f7;
-      font-family: 'Open Sans', sans-serif;
-      padding-top: 80px;
-    }
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-    .container {
+  <title>Contact Us | Community Harvest</title>
+
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" 
+    integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" />
+  <link rel="stylesheet" href="../../CSS/main.css">
+
+  <style>
+    /* .container {
       max-width: 600px;
       padding: 40px;
       border-radius: 8px;
       box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
       background-color: #fff;
       margin: 0 auto;
-    }
-
-    .contact-form h2 {
-      text-align: center;
-      margin-bottom: 20px;
-      color: #3c763d;
-      font-size: 24px;
     }
 
     .contact-form .form-group {
@@ -76,44 +65,61 @@
       background-color: #2d542e;
       border-color: #2d542e;
     }
+    */
 
     .alert {
       margin-top: 15px;
+    }
+
+    .container{
+      width: 35rem !important;
+    }
+
+    div h2{
+      color: #3c763d;
     }
   </style>
 </head>
 
 <body>
+  <?php include "header.php"; ?>
 
-  <div class="container contact-form">
+    <div class="container border border-2 rounded bg-white m-5 py-5">
+      <form method="post" action="#">
+        <div class="p-3">
+          <h2 class="text-center"><i class="fas fa-question-circle"></i> Contact Us</h2>
 
-    <body
-      style="background-image: url('container.png'); background-size: cover; background-repeat: no-repeat; background-position: center;">
-      <div class="container contact-form">
-        <h2><i class="fas fa-question-circle"></i> Contact Us</h2>
-        <form method="post" action="#">
           <div class="form-group">
             <label for="name">Full Name</label>
             <input type="text" class="form-control" id="name" name="name" placeholder="Enter your name">
+            <hr>
           </div>
+          
           <div class="form-group">
             <label for="email">Email Address</label>
             <input type="email" class="form-control" id="email" name="email" aria-describedby="emailHelp"
               placeholder="Enter your email">
+            <hr>
           </div>
-        <div class="form-group">
+            
+          <div class="form-group">
             <label for="message">Your Message</label>
-            <textarea class="form-control" id="message" name="message" rows="3"></textarea>
+            <textarea class="form-control" id="message" name="message" rows="3" placeholder="Enter your message">
+            </textarea>
+            <hr>
           </div>
-          <button type="submit" class="btn btn-primary">Send</button>
-        </form>
-      </div>
 
+          <button type="submit" class="btn btn-outline-success">Send</button>
+        </div>
+      </form>
+    </div>
+  </div>
 
+  <?php include "footer.php"; ?>
 
-      <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p"
-        crossorigin="anonymous"></script>
-    </body>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous">
+  </script>
+</body>
 
 </html>
--- Execution/PHP/login.php
+++ Execution/PHP/login.php
@@ -13,89 +13,85 @@ unset($_SESSION['success_msg']);
 
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login System</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
+    <title>Login | Cleckhuddersfax Market Hub</title>
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" 
+        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" />
-
-    <link rel="stylesheet" href="../CSS/styles.css">
+    <link rel="stylesheet" href="../CSS/form.css">
 </head>
 
 <body>
-    <form method="POST" id="customerLogin" action="login_process.php">
-        <img src="../Image/Logo.png" class="img-fluid rounded mx-auto d-block" height="50px" width="70px" alt="">
-        <h2 class="fw-bolder text_center mt-3 ">LOGIN</h2>
-        <div class="container border border-2 rounded color_white pb-4">
+    <div class="py-4">
+        <img src="../Image/Logo.png" class="img-fluid rounded mx-auto d-block" height="50px" width="70px" alt="Logo">
 
-            <div class="login_container mt-4">
-                <div class="Username">
-                    <label>Username</label>
-                    <div class="input-container">
+        <div class="container border border-2 rounded color_white mt-4 p-2">
+            <form method="POST" id="customerLogin" action="login_process.php">
+                <div class="login_container">
+                    <h2 class="fw-bolder text_center">LOGIN</h2>
+
+                    <div class="input-container Username">
                         <i class="fa-solid fa-envelope"></i>
                         <input type="text" class="text_box" placeholder="Email" name="email">
                         <hr>
                     </div>
-                </div>
 
-                <div class="password">
-                    <label>Password</label>
-                    <div class="input-container">
+                    <div class="input-container password">
                         <i class="icon fas fa-lock"></i>
                         <input type="password" class="text_box" placeholder="Password" name="password">
                         <hr>
                     </div>
+
+                    <div class="input-container user-type">
+                        <i class="icon fas fa-user"></i>
+                        <label for="userType">&nbsp;I am&nbsp;</label>
+                        <select class="text_box" id="userType" name="role">
+                            <option value="customer">Customer</option>
+                            <option value="trader">Trader</option>
+                        </select>
+                        <hr>
+                    </div>
+
+                    <label class="mb-2">
+                        <input type="checkbox">
+                        Remember Me
+                    </label>
+
+                    <div id="error_customerLogin">
+                        <?php
+                        // Display the error message if it's set
+                        if($successMsg){
+                            echo "<div class='alert alert-success' role='alert'>" . $successMsg . "</div>"; 
+                        }
+                        if ($errorMsg) {
+                            echo "<div class='alert alert-danger' role='alert'>" . $errorMsg . "</div>";
+                        }
+                        ?>
+                    </div>
+
+                    <div class="login_container mt-2 mb-0">
+                        <button type="submit" class="btn btn-outline-success rounded-5 mb-2">Login</button>
+
+                        <button type="button" class="btn btn-outline-success rounded-5 mb-2">
+                            <a href="Customer/customer_register.php" class="remove">Register As Customer</a>
+                        </button>
+                        <button type="button" class="btn btn-outline-success rounded-5">
+                            <a href="Trader/trader_registration.php" class="remove">Register As Trader</a>
+                        </button>
+                    </div>
+
+                    <a href="../PHP/Customer/reset_password.php" id="forgot_password"><small>Forgot Password?</small></a>
                 </div>
-
-                <div class="user-type mt-2 mb-3">
-                    <label for="userType" class="mb-2 fw-bolder">I am a:</label>
-                    <select class="form-select" id="userType" name="role">
-                        <option value="customer">Customer</option>
-                        <option value="trader">Trader</option>
-                    </select>
-                </div>
-
-                <label>
-                    <input type="checkbox">
-                    Remember Me
-                </label>
-
-                <!-- <label><u>Forgot Password?</u></label> -->
-                <a href="" id="forgot_password">Forgot Password?</a>
-
-
-            </div>
-
-            <div id="error_customerLogin">
-                <?php
-                // Display the error message if it's set
-                if($successMsg){
-                    echo "<div class='alert alert-success' role='alert'>" . $successMsg . "</div>"; 
-                }
-                if ($errorMsg) {
-                    echo "<div class='alert alert-danger' role='alert'>" . $errorMsg . "</div>";
-                }
-                ?>
-            </div>
-            <div class="login_container mt-3">
-                <button type="submit" class="btn btn-dark rounded-5 mb-4">Login</button>
-
-                <button type="button" class="btn btn-dark rounded-5 mb-4">
-                    <a href="Customer/register_customer.php" class="remove">Register As Customer</a>
-                </button>
-                <button type="button" class="btn btn-dark rounded-5">
-                    <a href="Trader/trader_registration.php" class="remove">Register As Trader</a>
-                </button>
-
-            </div>
-
+            </form>
         </div>
-    </form>
+    </div>
+
     <script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL"
-        crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" 
+        integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous">
+    </script>
 </body>
 
 </html>
